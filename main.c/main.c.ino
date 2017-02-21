3ad44913bd9213dcaa3fe9e2809cf8db995799c8
Unknown <fille044@gmail.com>
2017-02-21 12:46:15 +0100

    Link to documents

--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -14,7 +14,6 @@ DS3231 rtc(SDA,SCL);
 dht DHT;
 
 int old_hour;
-int i = 0;
 
 weather space[array_size];
 
@@ -40,20 +39,21 @@ void setup() {
 
 
 void loop() {
-  
-  for (int k=0;k<array_size;k++){
-    Serial.print(space[k].measured_hour);
-    Serial.print(" --- ");
-    Serial.print(space[k].temp);
-    Serial.print(" --- ");
-    Serial.print(space[k].humidity);
-    Serial.println(" --- ");
-  }
 
+  for (int k=0;k<array_size;k++){ 
+    Serial.print(space[k].measured_hour); 
+    Serial.print(" --- "); 
+    Serial.print(space[k].temp); 
+    Serial.print(" --- "); 
+    Serial.print(space[k].humidity); 
+    Serial.println(" --- "); 
+  } 
   
   int temp = DHT.read11(DHT11);
   //Transforms string of time to int, where only the hour is collected
   int new_hour = atoi(rtc.getTimeStr());
+  
+  static int i = 0;
   // If the hour variable has been changed, read new values
   if (new_hour != old_hour){
     space[i].measured_hour = new_hour;
@@ -62,7 +62,6 @@ void loop() {
     space[i].humidity = DHT.humidity;
     print_to_SD(space[i].measured_hour, space[i].temp, space[i].humidity);
     i++;
-    Serial.println(new_hour);
   }
 
   // If button is pressed
