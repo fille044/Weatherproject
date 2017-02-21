6dad3d97db35f379676208f8c04ff85f7c4e8ab1
Unknown <fille044@gmail.com>
2017-02-21 10:37:25 +0100

    blabla

--- main.c/library.h
+++ main.c/library.h
@@ -3,3 +3,8 @@ void print_to_serial(int measured_hour, int temp, int humidity);
 void print_to_SD(int measured_hour, int temp, int humidity);
 int print_high_temp(int space);
 
+typedef struct _weather_S{
+  int temp;
+  int humidity;
+  int measured_hour;
+} weather;
--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -16,26 +16,20 @@ dht DHT;
 int old_hour;
 int i = 0;
 
-typedef struct _weather_S{
-  int temp;
-  int humidity; 
-  int measured_hour;
-} weather;
 weather space[array_size];
-
 
 void setup() {
   Serial.begin(9600);
   pinMode(Button, INPUT);
   rtc.begin();
-  
+
   // wait for serial port to connect. Needed for native USB port only
   while (!Serial) {
-    Serial.println("Serial port connects"); 
+    Serial.println("Serial port connects");
   }
   // see if the card is present and can be initialized:
   Serial.print("Initializing SD card...");
-  
+
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
@@ -48,22 +42,22 @@ void setup() {
 void loop() {
   int temp = DHT.read11(DHT11);
   //Transforms string of time to int, where only the hour is collected
-  int new_hour = atoi(rtc.getTimeStr());  
-
+  int new_hour = atoi(rtc.getTimeStr());
   // If the hour variable has been changed, read new values
   if (new_hour != old_hour){
     space[i].measured_hour = new_hour;
     old_hour = new_hour;
     space[i].temp = DHT.temperature;
     space[i].humidity = DHT.humidity;
+    print_to_SD(space[i].measured_hour, space[i].temp, space[i].humidity);
     i++;
     Serial.println(new_hour);
   }
 
   // If button is pressed
   if (digitalRead(Button)){
-    for (int j=0 ; j<backlog ; j++){  
-      print_to_SD(space[j].measured_hour, space[j].temp, space[j].humidity); 
+    for (int j=0 ; j<backlog ; j++){
+      //print_to_SD(space[j].measured_hour, space[j].temp, space[j].humidity);
       print_to_serial(space[j].measured_hour, space[j].temp, space[j].humidity);
     }
   }
