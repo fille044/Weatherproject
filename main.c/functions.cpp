38e691e46360bf2a1fb3d4a1a4fb13ff49b47a8b
Unknown <fille044@gmail.com>
2017-02-22 10:24:41 +0100

    Inbefore final test

--- main.c/functions.cpp
+++ main.c/functions.cpp
@@ -8,7 +8,7 @@ File dataFile;
 // Prints the result to results.txt on SD-card
 void print_to_SD(int measured_hour, int temp, int humidity){
   // Opens file in write-mode and prints
-  dataFile = SD.open("test.txt", FILE_WRITE);
+  dataFile = SD.open("final.txt", FILE_WRITE);
   if(dataFile){
     dataFile.print("Hour: ");
     dataFile.print(measured_hour);
@@ -26,16 +26,16 @@ void print_to_SD(int measured_hour, int temp, int humidity){
 
 // Prints the same text to serial monitor
 void print_to_serial(void){
-  dataFile = SD.open("test.txt");
+  dataFile = SD.open("final.txt");   // Opens up test.txt
   if (dataFile){
-    Serial.println("Opening test.txt");
+    Serial.println("Opening final.txt");
     while (dataFile.available()){
       Serial.write(dataFile.read());
     }
     dataFile.close();
   }
   else{
-    Serial.println("Error reading from test.txt");
+    Serial.println("Error reading from final.txt");
   }
 
 }
@@ -43,9 +43,12 @@ void print_to_serial(void){
 
 void print_high_temp(weather* space, int array_size){ 
     int current, next, place;
-    int temporary = space[0].temp; 
+    int temporary = space[0].temp; // initiates temporary with the first value of array
     for (current = 0; current < array_size; current++) {
+      //Compares the value of temp with the value in temporary
       if (space[current].temp > temporary) { 
+        //  If the new value is higher than the last, save the new to 
+        //  temporary and save the position in the array
           temporary =  space[current].temp; 
           place = current;
       }
--- main.c/main.c.ino
+++ main.c/main.c.ino
@@ -7,12 +7,10 @@
 #define DHT11 2
 #define Button 8
 #define chipSelect 10
-#define backlog 6
 #define array_size 25
 
 DS3231 rtc(SDA,SCL);
 dht DHT;
-
 
 weather space[array_size];
 
@@ -34,55 +32,42 @@ void setup() {
     return;
   }
   Serial.println("card initialized.");
-  space[12].temp = 26;
-  space[12].measured_hour = 12;
-  space[13].temp = 20;
-  space[13].measured_hour = 13;
-  space[14].temp = 21;
-  space[14].measured_hour = 14;
-  space[17].temp = 24;
-  space[17].measured_hour = 17;
 }
 
-
 void loop() {
-
-  for (int k=0;k<array_size;k++){ 
-    Serial.print(space[k].measured_hour); 
-    Serial.print(" --- "); 
-    Serial.print(space[k].temp); 
-    Serial.print(" --- "); 
-    Serial.print(space[k].humidity); 
-    Serial.println(" --- "); 
-  } 
-  
   int temp = DHT.read11(DHT11);
   //Transforms string of time to int, where only the hour is collected
   int new_hour = atoi(rtc.getTimeStr());
   
-  static int i = 0;
   static int old_hour;
 
-    /*Serial.print(" *** "); 
-    Serial.print(i); 
-    Serial.println(" *** "); */
   // If the hour variable has been changed, read new values
  if (new_hour<old_hour || new_hour>old_hour){
     space[new_hour].measured_hour = new_hour;
     old_hour = new_hour;
     space[new_hour].temp = DHT.temperature;
     space[new_hour].humidity = DHT.humidity;
+    // Prints the values to SD-card
     print_to_SD(space[new_hour].measured_hour, space[new_hour].temp, space[new_hour].humidity);
-    i++;
+    //  Prints the whole array
+    for (int k=0;k<array_size;k++){ 
+      Serial.print(space[k].measured_hour); 
+      Serial.print(" --- "); 
+      Serial.print(space[k].temp); 
+      Serial.print(" --- "); 
+      Serial.print(space[k].humidity); 
+      Serial.println(" --- "); 
+    }
+    // Calculates the highest temperature the last 24h
+    print_high_temp(space, array_size); 
   }
 
   // If button is pressed
   if (digitalRead(Button)){
+    //  Reads from SD and prints to serial
     print_to_serial();
     Serial.println("----------------------");
   }
 
-  print_high_temp(space, array_size); 
-  
   delay(10000);
 }
